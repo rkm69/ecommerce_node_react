2c3bc9b8d59801747ee41d37dc24daacfd8b7e19
thdarkarmy <thepenseur1729@gmail.com>
2020-07-19 12:47:56 +0530

    Testin app purpose

---
+++ backend/data.js
@@ -0,0 +1,40 @@
+export default { 
+    products: [
+    {  
+        _id : '1',
+        name :'7x5 ganesh',
+        category : 'Rectangle Design', 
+        image:'/sample_images/ganeshsample.jpeg',
+        price: 88,
+        Moq:20,
+        countInStock:100,
+    },
+    {  
+        _id :'2',
+        name :'7x5 Abhi',
+        category : 'Rectangle Design', 
+        image:'/sample_images/ganeshsample.jpeg',
+        price: 88,
+        Moq:20,
+        countInStock:100,
+    },
+    {  
+        _id :'3',
+        name :'6x4 Neha',
+        category : 'Rectangle Design', 
+        image:'/sample_images/ganeshsample.jpeg',
+        price: 88,
+        Moq:20,
+        countInStock:100,
+    },
+    {  
+        _id :'4',
+        name :'3x5 kuhoo',
+        category : 'Rectangle Design', 
+        image:'/sample_images/ganeshsample.jpeg',
+        price: 88 ,
+        Moq:35,
+        countInStock:100,
+    },
+    ]
+}
